25d20b28298091de2d382751ef77fb415b241fc1
DmitriyKubay <uakubaydm@gmail.com>
2020-01-29 22:20:17 +0200

    Commit while

---
+++ dz_while.py
@@ -0,0 +1,8 @@
+
+a = int(input("Enter the number A:"))
+b = int(input("Enter the number B:"))
+c = int(input("Enter the number C:"))
+while a <= b:
+    a += c
+else:
+    print ("Well done! A=", a)
